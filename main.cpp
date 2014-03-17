c274eba67b10e2705a52b8a352f71707f9ae114f
Nick <zuzownic@msu.edu>
2014-03-17 12:30:11 -0400

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -18,15 +18,47 @@ using namespace std;
 int main()
 {
 
-Expr* ex= new And(new Literal(true), new Literal(true));
+    Expr* ex= new And(new Literal(true), new Literal(false));
 
-cout << ex->compute() << endl;
+    Expr* ex2 = new Or(new Literal(true), new Literal(false));
 
-EvaluationVisitor ev;
-PrintVisitor pv;
-ex->accept(&ev);
-ex->accept(&pv);
+    Expr* eximp = new Implication(ex, ex2);
+    
+    Expr* exnot = new Negate(ex2);
+    
+    Expr* exequal = new Equivalence(exnot, new Literal(true));
+    
+    EvaluationVisitor ev;
+    PrintVisitor pv;
+    
+    //call accept of And
+    ex->accept(&ev);
+    ex->accept(&pv);
 
-delete ex;
-cout << endl;
+    cout << endl;
+    cout << ex->compute() << endl;
+    
+    //call accept of Or
+    ex2->accept(&ev);
+    ex2->accept(&pv);
+    
+    cout << endl;
+    cout << ex2->compute() << endl;
+    
+    //call accept of Implication
+    eximp->accept(&ev);
+    eximp->accept(&pv);
+    
+    cout << endl;
+    cout << eximp->compute() << endl;
+    
+    //call accept of Equivalence
+    exequal->accept(&ev);
+    exequal->accept(&pv);
+    
+    cout << endl;
+    cout << exequal->compute() << endl;
+    
+    delete ex;
+    cout << endl;
 }
